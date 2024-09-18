9faeffa3fa28a4c49afe8eed842a1f2162df48f0
wnsrl3 <144307859+wnsrl3@users.noreply.github.com>
2024-09-18 22:43:46 +0900

    Title: 그 전 알파벳, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240918/그 전 알파벳/before-the-alphabet.py
@@ -0,0 +1,14 @@
+def min_shift_to_match(A, B):
+    len_a = len(A)
+
+    for n in range(1, len_a + 1):
+        shifted_A = A[-n:] + A[:-n]
+        if shifted_A == B:
+            return n
+    
+    return -1
+
+A = input().strip()
+B = input().strip()
+
+print(min_shift_to_match(A, B))
