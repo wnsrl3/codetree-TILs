c5931e27a0f799f2c4877d83085e46a9e6c9c643
wnsrl3 <144307859+wnsrl3@users.noreply.github.com>
2024-10-06 14:22:29 +0900

    Title: 공약수의 유무, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 241006/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 241006/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,8 +1,10 @@
 a,b=map(int,input().split())
-
+check=0
 for n in range(a,(b+1)):
     if 1920%n==0 and 2880%n==0 :
-        print(1)
+        check=1
         break
     else :
-        print(0)
+        check=0
+
+print(check)
